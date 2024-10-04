913ba16e48434cd5bacb5cc8a7f178aed77f37e0
Divya Maurya <divyamaurya025@gmail.com>
2024-10-04 21:21:44 +0530

    Router Component

---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,40 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from "./Layout";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       { path: "", element: <Home /> },
+//       { path: "about", element: <About /> },
+//       { path: "contact", element: <Contact /> },
+//     ],
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+    </Route>
+  )
+);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
